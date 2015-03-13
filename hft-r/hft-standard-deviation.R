c5b4ce1608e460d62b94d9dce48db8f2019cf21c
Pierre Bartholomä <pierre.bartholomae@exxeta.com>
2015-03-13 09:47:07 +0100

    update standard deviation text sizing

--- hft-r/hft-standard-deviation.R
+++ hft-r/hft-standard-deviation.R
@@ -37,6 +37,8 @@ plotBars <- function (standardDeviationTableData) {
   
   startDate <- as.numeric(Sys.time());
   
+  zoomFactor <- 1.25
+  
   color1 <- "#b33438"
   color2 <- "#0076e7"
   color3 <- "#31a84a"
@@ -49,20 +51,21 @@ plotBars <- function (standardDeviationTableData) {
   image <- barplot(data, col=c(color1,color2,color3),
                    ylim=c(0,0.11), legend=TRUE,
                    xlab="Execution/Side", ylab="Deviation",
-                   beside=TRUE, space=c(0.2,0.75), xaxt="n")
-  
-  #text(image, dataList-0.00125, labels=dataList, col="black", cex=1)
-  
-  axis(1, at=c(2.5,6.5,11), label=lineHeader)
-  
-  #title(main="Standard deviation", col.main="black", font.main=4)
+                   beside=TRUE, space=c(0.2,0.75),
+                   xaxt="n", yaxt="n",
+                   cex.lab=zoomFactor, cex.axis=zoomFactor, cex.main=zoomFactor, cex.sub=zoomFactor)
+    
+  axis(1, at=c(2.5,6.5,11), label=lineHeader, cex.axis=zoomFactor)
+  axis(2, cex.axis=zoomFactor)
   
   labelFull <- "Full Data"
   labelHFT <- "HFT Data"
   labelnonHFT <- "Non HFT Data"
   
-  legend("topright", c(labelFull, labelHFT, labelnonHFT), fill=c(color1,color2,color3),
-         inset=.05, bty="n")
+  legend("topright",
+         c(labelFull, labelHFT, labelnonHFT),
+         fill=c(color1,color2,color3),
+         pt.cex=1, cex=zoomFactor, bty="n")
   
   endDate <- as.numeric(Sys.time())
   plottingDuration <- endDate-startDate
